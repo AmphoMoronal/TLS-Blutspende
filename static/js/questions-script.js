75f8b11a1028e1556807de193cfc55827524dc64
Luis Herr <amphomoronal@gmail.com>
2023-09-11 19:51:25 +0200

    formatted

--- static/js/questions-script.js
+++ static/js/questions-script.js
@@ -1,44 +1,40 @@
 function toggleCheck(checkbox) {
-        // Suchen Sie das übergeordnete <div> des geänderten Kontrollkästchens
-        var parentDiv = checkbox.closest('.card');
+    // Suchen Sie das übergeordnete <div> des geänderten Kontrollkästchens
+    let parentDiv = checkbox.closest('.card');
 
-        // Durchlaufen Sie die Checkboxen innerhalb des übergeordneten <div>
-        var checkboxes = parentDiv.querySelectorAll('input[type="checkbox"]');
-        for (var i = 0; i < checkboxes.length; i++) {
-            if (checkboxes[i] !== checkbox) {
-                checkboxes[i].checked = false; // Anderes Kontrollkästchen abwählen
+    // Durchlaufen Sie die Checkboxen innerhalb des übergeordneten <div>
+    let checkboxes = parentDiv.querySelectorAll('input[type="checkbox"]');
+    for (const element of checkboxes) {
+        if (element !== checkbox) {
+            element.checked = false; // Anderes Kontrollkästchen abwählen
+        }
+    }
+}
+
+function checkButtons() {
+    let cards = document.querySelectorAll('.card');
+    let button = document.getElementById('weiterbtn');
+    let allCardsHaveCheckboxChecked = true;
+
+    // Überprüfen, ob in jedem .card-Div mindestens eine Checkbox ausgewählt ist
+    for (const element of cards) {
+        let checkboxes = element.querySelectorAll('input[type="checkbox"]');
+        let atLeastOneChecked = false;
+
+        // Überprüfen, ob mindestens eine Checkbox in diesem .card-Div ausgewählt ist
+        for (let j of checkboxes.length) {
+            if (checkboxes[j].checked) {
+                atLeastOneChecked = true;
+                break; // Wenn mindestens eine Checkbox ausgewählt ist, abbrechen
             }
+        }
+
+        if (!atLeastOneChecked) {
+            allCardsHaveCheckboxChecked = false;
+            break; // Wenn mindestens ein .card-Div keine ausgewählte Checkbox hat, abbrechen
         }
     }
 
-    function checkButtons() {
-            var cards = document.querySelectorAll('.card');
-            var button = document.getElementById('weiterbtn');
-            var allCardsHaveCheckboxChecked = true;
-
-            // Überprüfen, ob in jedem .card-Div mindestens eine Checkbox ausgewählt ist
-            for (var i = 0; i < cards.length; i++) {
-                var checkboxes = cards[i].querySelectorAll('input[type="checkbox"]');
-                var atLeastOneChecked = false;
-
-                // Überprüfen, ob mindestens eine Checkbox in diesem .card-Div ausgewählt ist
-                for (var j = 0; j < checkboxes.length; j++) {
-                    if (checkboxes[j].checked) {
-                        atLeastOneChecked = true;
-                        break; // Wenn mindestens eine Checkbox ausgewählt ist, abbrechen
-                    }
-                }
-
-                if (!atLeastOneChecked) {
-                    allCardsHaveCheckboxChecked = false;
-                    break; // Wenn mindestens ein .card-Div keine ausgewählte Checkbox hat, abbrechen
-                }
-            }
-
-            // Button aktivieren oder deaktivieren und Design ändern
-            if (allCardsHaveCheckboxChecked) {
-                button.disabled = false;
-            } else {
-                button.disabled = true;
-            }
-        }
+    // Button aktivieren oder deaktivieren und Design ändern
+    button.disabled = !allCardsHaveCheckboxChecked;
+}
