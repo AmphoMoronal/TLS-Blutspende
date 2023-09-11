bc9ec4eaaccf20ec3f4e8313aef9872655e0c1a1
Luis Herr <amphomoronal@gmail.com>
2023-09-11 20:28:32 +0200

    fixed bugs

--- appointment.py
+++ appointment.py
@@ -4,7 +4,7 @@ from db import session, Appointments
 """
 
 The appointments are always from 10:00 a.m. to 3:00 p.m. 
-You can process 4 donors every quarter. -> 16 donaters per hour -> 50 donators per appointment
+You can process 4 donors every quarter. -> 16 donors per hour -> 50 donors per appointment
 
 """
 
@@ -23,17 +23,17 @@ class Appointment:
         return appointment
 
     @staticmethod
-    def get_one(time):
-        appointment = session.query(Appointments).filter(Appointments.time == time).first()
+    def get_one(date, time):
+        appointment = session.query(Appointments).filter(Appointments.date == date, Appointments.time == time).first()
         if not appointment:
             return None
 
         return appointment
 
     @staticmethod
-    def get_dates():
+    def get_appointment(date):
         dates = []
-        appointments = session.query(Appointments).all()
+        appointments = session.query(Appointments).filter(Appointments.date == date).all()
         for appointment in appointments:
             if appointment.date not in dates:
                 dates.append(appointment.date)
@@ -52,3 +52,21 @@ class Appointment:
                 termin = Termin(date=date, time=time)
                 session.add(termin)
             session.commit()
+
+    @staticmethod
+    def delete_appointment(date):
+        appointments = session.query(Appointments).filter(Appointments.date == date).all()
+        if appointments:
+            for appointment in appointments:
+                session.delete(appointment)
+            session.commit()
+
+    @staticmethod
+    def reset_time(date, time):
+        appointment = session.query(Appointments).filter(Appointments.date == date, Appointments.time == time).first()
+        if not appointment:
+            return None
+
+        return appointment
+
+
