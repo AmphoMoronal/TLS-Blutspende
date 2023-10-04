bc0355709450f5935aed2a2cd1811ae16b618b2d
Luis Herr <78873853+AmphoMoronal@users.noreply.github.com>
2023-10-04 17:37:19 +0200

    added basic appointment overview (#27)
    * Added basic Admin page
    
    * list latest appointment

--- app.py
+++ app.py
@@ -157,7 +157,7 @@ def logout():
 @app.route("/questions")
 def questions():
     if current_user.is_authenticated:
-        return render_template("questions.html")
+        return render_template("questions.html", current_user=current_user)
 
     else:
         return redirect(url_for("index"))
@@ -211,13 +211,23 @@ def set_appointment():
         time = request.args.get('time'),
         date = Appointment.get_date()[0]
         Appointment.add_doner(date, time[0], current_user.user_id)
-        utils.send_confirmation_email(current_user, date, time[0])
+        if not current_user.appointment:
+            utils.send_confirmation_email(current_user, date, time[0])
         return render_template("confirmation.html", time_slot=time[0], date=date,
                                current_user=current_user)
 
     else:
         return redirect(url_for("index"))
 
+
+@app.route("/admin")
+def admin():
+    if current_user.admin:
+        data = Appointment.get_appointment(Appointment.get_dates()[-1])
+        return render_template("admin.html", data=data, doner=Doner.get)
+
+    else:
+        return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(ssl_context="adhoc", port=5000, use_reloader=True)
